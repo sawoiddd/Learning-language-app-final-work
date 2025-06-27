0c124786a0c1617974f81f5ce34facc30eb8ef60
Sasha Muzyka <sacha.muzika@gmail.com>
2025-06-27 17:39:01 +0300

    Create DAL, DataContext.cs, WordRepository

---
+++ LearningLanguageApp/LearningLanguageApp.DAL/LearningLanguageAppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LearningLanguageApp.DAL;
+
+public class LearningLanguageAppDataContext : DbContext
+{
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LearningLanguageAppDB;Integrated Security=True");
+    }
+
+    public DbSet<Word> Words { get; set; }
+
+}
---
+++ LearningLanguageApp/LearningLanguageApp.DAL/Repositories/WordRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningLanguageApp.DAL.Repositories;
+
+public class WordRepository : IWordRepository
+{
+    private readonly LearningLanguageAppDataContext _context;
+    public WordRepository(LearningLanguageAppDataContext context)
+    {
+        _context = context;
+    }
+
+
+
+}
