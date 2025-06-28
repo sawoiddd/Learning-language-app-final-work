2407ad53ec5c01d7c7e0dd618bc4418f73d2d325
oket23 <oket23@DESKTOP-DGUVS6I>
2025-06-28 15:19:05 +0300

    add LoggerService

---
+++ LearningLanguageApp/LearningLanguageApp/Services/LoggerService.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+
+namespace LearningLanguageApp.Services;
+
+public class LoggerService
+{
+    public static ILogger GetLogger()
+    {
+        var logPath = "Logs/Logs.txt";
+
+        var logDirectory = Path.GetDirectoryName(logPath);
+        Directory.CreateDirectory(logDirectory);
+
+        return new LoggerConfiguration()
+               .WriteTo.File(logPath, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}]: {Message:lj}{NewLine}{Exception}")
+               .MinimumLevel.Debug()
+               .CreateLogger();
+    }
+}
