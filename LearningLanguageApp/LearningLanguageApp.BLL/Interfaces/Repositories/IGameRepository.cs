759082b8359bb346c67d0036118c718bd45e7917
Oleg Golub <oleggolub460@gmail.com>
2025-06-30 02:01:56 +0300

    Delete LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IGameRepository.cs

--- LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IGameRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using LearningLanguageApp.BLL.Models;
-
-namespace LearningLanguageApp.BLL.Interfaces.Repositories;
-
-public interface IGameRepository
-{
-    Task<IList<Word>> GetRandomWordsAsync(int userId, CancellationToken cancellationToken);
-}
