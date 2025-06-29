ccf820f5fb430f53ca6a5692bc73f4bf152a21f5
oket23 <oleggolub460@gmail.com>
2025-06-30 02:09:54 +0300

    Created IGameRepository

--- LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IGameRepository.cs
+++ LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IGameRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace LearningLanguageApp.BLL.Interfaces.Repositories;
+﻿using LearningLanguageApp.BLL.Models;
+
+namespace LearningLanguageApp.BLL.Interfaces.Repositories;
 
 public interface IGameRepository
 {
-
+    Task<IList<Word>> GetRandomWordsByDictionaryAsync(int dictionaryId, int count, CancellationToken cancellationToken);
 }
