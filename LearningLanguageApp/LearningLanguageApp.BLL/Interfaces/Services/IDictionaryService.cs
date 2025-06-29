56bb647e7b01519459fd1ff69fc640f6bcc7096e
oket23 <oleggolub460@gmail.com>
2025-06-30 01:14:18 +0300

    Added IDictionaryService

---
+++ LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Services/IDictionaryService.cs
@@ -0,0 +1,12 @@
+﻿using LearningLanguageApp.BLL.Models;
+
+namespace LearningLanguageApp.BLL.Interfaces.Services;
+
+public interface IDictionaryService
+{
+    Task<Dictionary> GetDictionaryByIdAsync(int dictionaryId, CancellationToken cancellationToken);
+    Task<IEnumerable<Dictionary>> GetUserDictionariesAsync(int userId, CancellationToken cancellationToken);
+    Task<Dictionary> CreateDictionaryAsync(Dictionary dictionary, int userId, CancellationToken cancellationToken);
+    Task<Dictionary> UpdateDictionaryAsync(Dictionary dictionary, CancellationToken cancellationToken);
+    Task<Dictionary> DeleteDictionaryAsync(int dictionaryId, CancellationToken cancellationToken);
+}
