db8cbcd6ad32b722326449d4a058cb97f8a91c68
oket23 <oleggolub460@gmail.com>
2025-06-30 13:53:20 +0300

    fix 2

--- LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IDictionaryRepository.cs
+++ LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IDictionaryRepository.cs
@@ -7,7 +7,7 @@ public interface IDictionaryRepository
 {
     Task<Dictionary> GetDictionaryByIdAsync(int dictionaryId, CancellationToken cancellationToken);
     Task<IEnumerable<Dictionary>> GetByUserDictionaryIdAsync(int userId, CancellationToken cancellationToken);
-    Task<Dictionary> AddDictionaryAsync(AddDictionaryDto dictionary, CancellationToken cancellationToken);
+    Task<Dictionary> AddDictionaryAsync(Dictionary dictionary, CancellationToken cancellationToken);
     Task<Dictionary> UpdateDictionaryAsync(Dictionary dictionary, CancellationToken cancellationToken);
     Task<Dictionary> DeleteDictionaryAsync(int dictionaryId, CancellationToken cancellationToken);
 }
