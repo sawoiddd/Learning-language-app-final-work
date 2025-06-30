74648ab7307af1c938d87b63155819657e1e5acd
oket23 <oleggolub460@gmail.com>
2025-06-30 14:05:27 +0300

    fix

---
+++ LearningLanguageApp/LearningLanguageApp.BLL/Dtos/UpdateWordDto.cs
@@ -0,0 +1,11 @@
+﻿using LearningLanguageApp.BLL.Enums;
+
+namespace LearningLanguageApp.BLL.Dtos;
+
+public class UpdateWordDto
+{
+    public string OriginalWord { get; set; }
+    public string Translation { get; set; }
+    public WordTypeEnum Type { get; set; }
+    public WordLevelEnum Level { get; set; }
+}
--- LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Services/IWordService.cs
+++ LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Services/IWordService.cs
@@ -1,11 +1,12 @@
-﻿using LearningLanguageApp.BLL.Models;
+﻿using LearningLanguageApp.BLL.Dtos;
+using LearningLanguageApp.BLL.Models;
 
 namespace LearningLanguageApp.BLL.Interfaces.Services;
 
 public interface IWordService
 {
-    Task<Word> AddWordAsync(int dictionaryId, Word Word, CancellationToken cancellationToken);
-    Task<Word> UpdateWordAsync(Word word, CancellationToken cancellationToken);
+    Task<Word> AddWordAsync(int dictionaryId, AddWordDto dto, CancellationToken cancellationToken);
+    Task<Word> UpdateWordAsync(UpdateWordDto word, CancellationToken cancellationToken);
     Task<Word> DeleteWordAsync(int wordId, CancellationToken cancellationToken);
     Task<Word> MarkAsLearnedAsync(int wordId, CancellationToken cancellationToken);
     Task<IEnumerable<Word>> GetWordsAsync(int dictionaryId, CancellationToken cancellationToken);
