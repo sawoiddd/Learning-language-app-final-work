77c58bb5a2771883426d5a77cc65fa8c68297580
oket23 <oleggolub460@gmail.com>
2025-06-30 16:44:06 +0300

    added IGoogleTranslateRepository

---
+++ LearningLanguageApp/LearningLanguageApp.BLL/Interfaces/Repositories/IGoogleTranslateRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace LearningLanguageApp.BLL.Interfaces.Repositories;
+
+public interface IGoogleTranslateRepository
+{
+    Task<string> GetWordTranslateAsync(string originalWord, CancellationToken cancellationToken);
+}
